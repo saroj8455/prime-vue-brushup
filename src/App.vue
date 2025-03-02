c68346e3c25654a389828eff01ebd59206a90974
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2025-03-02 10:01:50 +0530

    define components in main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
 	<h1>Prime Vue Tutorial</h1>
-	<div class="card flex justify-content-center">
+	<div class="card flex flex-column gap-2 w-5 justify-content-center">
 		<Button label="Check" icon="pi pi-check" />
+		<Calendar v-model="date" />
 	</div>
 	<!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+export default {
+	data() {
+		return {
+			date: null,
+		};
+	},
+};
 </script>
