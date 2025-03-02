dfe792d1749d7b4b5ca910215be5d322969e5a7b
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2025-03-02 09:53:54 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<h1>Prime Vue Tutorial</h1>
+	<div class="card flex justify-content-center">
+		<Button label="Check" icon="pi pi-check" />
+	</div>
+	<!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
---
+++ src/router.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+
+export const router = createRouter({
+	history: createWebHistory(),
+	routes: [{ path: '/', component: App }],
+});
